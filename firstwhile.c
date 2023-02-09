161d882ecfd93931a4e5d9df0ffed38ead34a5c3
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-09 14:13:04 +0000

    firstwhile started

---
+++ firstwhile.c
@@ -0,0 +1,19 @@
+//
+// Created by rodrigo on 2/9/23.
+//
+//Write a program that reads from the keyboard a sequence of integers (number by number) and
+//end with a letter, any letter stops the sequence, use while( (scanf(“%d”, &n))==1 ) and calculate:
+//1.1 How many numbers were read;
+//1.2 Which is the last largest and in what relative position within the read sequence it is found.
+//1.3 Total sign changes from positive number to negative number;
+//1.4 How many read numbers form the first largest increasing subsequence;
+
+#include "stdio.h"
+
+int main(){
+    int n;
+
+
+    return 0;
+}
+
